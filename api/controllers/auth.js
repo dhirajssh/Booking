f51bfc44be55e3d5ce498e568841432cd6280cbf
Dhiraj Shah <dhirajssh@gmail.com>
2023-03-11 18:52:52 +0530

    jwt added

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
 import { createError } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const register = async(req, res, next) => {
   try{
@@ -27,9 +28,16 @@ export const login = async(req, res, next) => {
     if(!user) return next(createError(404, "User not found!"))
 
     const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
+
     if(!isPasswordCorrect) return next(createError(400, "Wrong password or username!"))
 
-    res.status(200).json(user);
+    const token = jwt.sign({id: user._id, isAdmin: user.isAdmin}, "sdfsdfs");
+
+    const {password, isAdmin, ...otherDetails} = user._doc;
+
+    res.cookie("access_token", token, {
+      httpOnly: true,
+    }).status(200).json({...otherDetails});
   } catch(error){
     next(error);
   }
