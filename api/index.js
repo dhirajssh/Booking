71378613581802b531c2b0dfdb99e8e7c3a8a2b5
Dhiraj Shah <dhirajssh@gmail.com>
2023-03-11 19:19:28 +0530

    user controllers and routes

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ app.use(cookieParser())
 app.use(express.json());
 
 app.use("/api/auth", authRoute)
-app.use("/api/users", authRoute)
+app.use("/api/users", usersRoute)
 app.use("/api/hotels", hotelsRoute)
 app.use("/api/rooms", authRoute)
 
