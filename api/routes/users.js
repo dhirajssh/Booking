aaed1386bdc4c72e65a8da2882bea27e915339d0
Dhiraj Shah <dhirajssh@gmail.com>
2023-03-11 20:04:34 +0530

    jwt tested

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,12 @@
 import express from "express"
 import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user.js";
+import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+router.get("/checkauthentication", verifyToken, (req,res,next)=>{
+  res.send("Hello user, you are logged in.")
+})
 
 // Update
 router.put("/:id", updateUser);
